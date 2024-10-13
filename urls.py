cbc4eca342cc5861ac317095b8e221804cbeffde
MAINAK_SAHA <84928246+MAINAKSAHA07@users.noreply.github.com>
2024-10-13 13:53:07 -0700

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,27 +1,23 @@
-"""capstone URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include("flight.urls")),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register_view, name="register"),
+    path("query/places/<str:q>", views.query, name="query"),
+    path("flight", views.flight, name="flight"),
+    path("review", views.review, name="review"),
+    path("flight/ticket/book", views.book, name="book"),
+    path("flight/ticket/payment", views.payment, name="payment"),
+    path('flight/ticket/api/<str:ref>', views.ticket_data, name="ticketdata"),
+    path('flight/ticket/print',views.get_ticket, name="getticket"),
+    path('flight/bookings', views.bookings, name="bookings"),
+    path('flight/ticket/cancel', views.cancel_ticket, name="cancelticket"),
+    path('flight/ticket/resume', views.resume_booking, name="resumebooking"),
+    path('contact', views.contact, name="contact"),
+    path('privacy-policy', views.privacy_policy, name="privacypolicy"),
+    path('terms-and-conditions', views.terms_and_conditions, name="termsandconditions"),
+    path('about-us', views.about_us, name="aboutus"),
+]
